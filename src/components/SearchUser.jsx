753e18f8b4e7cb698b81b4fe7239375513df0f39
@Nurul97 <nurulislamnextgenitltd.com>
2023-10-29 22:58:17 +0600

    title

--- src/components/SearchUser.jsx
+++ src/components/SearchUser.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import user from "../assets/image/usrrrr.png"
 
 function SearchUser() {
   return (
-    <div className='max-w-[1240px] mx-auto'>
-        <h1 className='text-center font-bold'>SearchUser</h1>
+    <div className='max-w-[1240px] mx-auto mt-5'>
+        <div className='grid place-content-center'>
+            <img src={user} alt="user" className='my-5 w-[100px] h-[100px]'/>
+        </div>
+
+        <div className='grid place-content-center'>
+            <h1 className='text-3xl md:text-4xl font-mono font-bold'>Search Github User</h1>
+        </div>
     </div>
   )
 }
