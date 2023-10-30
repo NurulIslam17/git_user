96a8cba651c20b77e58d10d01d5af014c525684b
@Nurul97 <nurulislamnextgenitltd.com>
2023-10-30 22:29:06 +0600

    Search Input

--- src/components/SearchUser.jsx
+++ src/components/SearchUser.jsx
@@ -11,6 +11,20 @@ function SearchUser() {
         <div className='grid place-content-center'>
             <h1 className='text-3xl md:text-4xl font-mono font-bold'>Search Github User</h1>
         </div>
+
+        <div className='grid place-content-center mt-3'>
+          <input  type="text" placeholder='Enter Github User Name' className='bg-[#e3ebe3] rounded-lg border-4 p-2 w-[280px] md:w-[400px]'/>
+        </div>
+
+
+       
+        {/* <div className='flex justify-center mt-5'>
+          <div className='h-[200px] w-[160px] bg-slate-500 grid place-content-center'>
+              <img src={user} alt="" srcset="" />
+          </div>
+          <div className=' h-[200px] w-[400px] bg-slate-900'></div>
+        </div> */}
+
     </div>
   )
 }
