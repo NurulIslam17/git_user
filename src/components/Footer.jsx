c31163ebd63c9890c1cbd80dbe7da8286aae7572
@Nurul97 <nurulislamnextgenitltd.com>
2023-10-31 22:49:49 +0600

    Footer edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='flex flex-wrap justify-center gap-4 py-3 bg-slate-200  text-[20px] md:xl  md:bg-slate-200'>
-        <h1>&copy; All Right Reserved By <a className='font-bold' href="https://react-tailwind-sepia.vercel.app/" target='_blank'>DevSoft</a>.</h1>
+    <div className='flex flex-wrap justify-center gap-4 py-3 bg-slate-200  text-[20px] md:text-[17px]  md:bg-slate-200'>
+        <h1><strong>&copy;</strong> All Right Reserved By <a className='font-bold' href="https://react-tailwind-sepia.vercel.app/" target='_blank'>DevSoft</a>.</h1>
     </div>
   )
 }
